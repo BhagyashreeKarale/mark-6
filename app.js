dcdbe262d62b62c58507721310e13a073412b648
Bhagyashree Karale <karale.bhagyashree07@gmail.com>
2021-11-28 02:29:37 +0530

    taking input and console it

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-// console.log('Just checking if Js is working when imported form another file-Externally');
+var btnTranslate = document.querySelector('#btn-translate');
+var txtInput = document.querySelector('#txt-input');
 
-// alert('Just checking if alert works')
+console.log(txtInput);
+function clickHandler() {
+    //we don't need return function clickEventHandler() so cut it off
+    // return function clickEventHandler() { 
+        console.log('clicked'); 
+    // };
+    console.log("input", txtInput.value);
+}
 
-// input
-var username = prompt('Just checking if prompt works')
+btnTranslate.addEventListener('click',clickHandler)
 
+//note we don't need parenthesis along with funciton when we are passing a funciton as an callback function
 
-// processing
-var welcomeMessage = "Welcome " + username +" I'm 'just checking if alert works"
+//we have to define the callback funciton before using it, so move the callback funciton declaration above btnTranslate.addEventListener('click',clickHandler)
+ 
 
-// output
-alert(welcomeMessage)
+// function clickHandler() {
+//     //we don't need return function clickEventHandler() so cut it off
+//     // return function clickEventHandler() { 
+//         console.log('clicked'); 
+//     // };
+// }
