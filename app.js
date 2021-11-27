33afc8d9622609029bd5472039b372cb6ed9d475
Bhagyashree Karale <karale.bhagyashree07@gmail.com>
2021-11-27 07:47:06 +0530

    import JS

---
+++ app.js
@@ -0,0 +1,13 @@
+// console.log('Just checking if Js is working when imported form another file-Externally');
+
+// alert('Just checking if alert works')
+
+// input
+var username = prompt('Just checking if prompt works')
+
+
+// processing
+var welcomeMessage = "Welcome " + username +" I'm 'just checking if alert works"
+
+// output
+alert(welcomeMessage)
